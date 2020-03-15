e113656af66a7425feea1960043f865d22fcdb62
Guido Scialfa <dev@guidoscialfa.com>
2020-03-15 22:29:44 +0100

    Remove file header summary docblock

--- src/Factory.php
+++ src/Factory.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of the bem package.
- *
- * (c) Guido Scialfa <dev@guidoscialfa.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 declare(strict_types=1);
 
 namespace Widoz\Bem;
